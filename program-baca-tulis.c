784c268ba03a95b67eec58db07c1eccd1585a087
Andhikaprt <107765982+Dhikaweb7@users.noreply.github.com>
2022-10-17 15:14:25 +0700

    Create program-baca-tulis.c

---
+++ program-baca-tulis.c
@@ -0,0 +1,10 @@
+/* Program baca tulis 3 */
+#include<stdio.h>
+int main(){
+  int bilangan; // variabel bilangan ber tipe data integer
+  scanf("%d", &bilangan); /* masukan */
+  printf("Bilangan yang dibaca bernilai: "); // cetak masukan 
+  printf("%d.\n", bilangan); // cetak 
+  return 0;
+}
+   
