d7ae59cca38f14a5b23d25dee05e73737b613d35
Andhikaprt <107765982+Dhikaweb7@users.noreply.github.com>
2022-10-17 15:08:41 +0700

    Create hello-world.c

---
+++ hello-world.c
@@ -0,0 +1,9 @@
+ /* Program pertamaku: */
+ /* Mencetak "Halo Dunia!" */
+ /* X TKJ 1 | SMK RADEN PAKU */
+ 
+ #include <stdio.h>
+   int main(){  // Fungsi utama program
+      printf("Halo Dunia!\n"); // cetak Halo Dunia! ke layar
+     return 0; // program berjalan dengan benar
+    }
